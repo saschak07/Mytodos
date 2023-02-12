f1f4059b4e7729eeef590c99906925b1c7ae31f8
saschak07 <saschak07@gmail.com>
2023-02-12 12:09:38 +0100

    resolving cross origin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require ('express')
 const {getTodos,insertTodo,updateTodo,getOneTodo,removeTodo} = require('./db/toDoRepository')
-
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.get('/todos', getTodos)
 app.get('/todo/:id',getOneTodo)
 app.post('/todos', insertTodo)
